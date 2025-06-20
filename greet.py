83ce9af900393185d480686dec2fc60e1b18a478
Nichila de Mel <nichilademel@gmail.com>
2025-06-20 14:17:12 +0530

    modified

--- greet.py
+++ greet.py
@@ -1,6 +1,6 @@
 def greet(name):
-    return f"Hello, {name}! Welcome to GitHub."
+    return f"Hello, {name}! Welcome to GitHub." #modification
 
 if __name__ == "__main__":
     user = input("Enter your name:")
-    print(greet(user))
+    print(greet(user)) 
