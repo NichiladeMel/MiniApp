de5d959bb9232eb85dd159e1c1ad22ea675a3242
Nichila de Mel <nichilademel@gmail.com>
2025-06-20 14:22:37 +0530

    Added timestamp to greeting

--- greet.py
+++ greet.py
@@ -1,6 +1,16 @@
+import datetime
+
+#def greet(name):
+#    return f"Hello, {name}! Welcome to GitHub." #modification
+
+#if __name__ == "__main__":
+#    user = input("Enter your name:")
+#    print(greet(user)) 
+
 def greet(name):
-    return f"Hello, {name}! Welcome to GitHub." #modification
+    now = datetime.datetime.now()
+    return f"Hello, {name}! Current time is {now:%Y-%m-%d%H:%M:%S}"
 
 if __name__ == "__main__":
-    user = input("Enter your name:")
-    print(greet(user)) 
+    user = input("Enter your name: ")
+    print(greet(user))
