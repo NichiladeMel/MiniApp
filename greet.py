2c7f19966790a4d2952d0ffa8b5c12c767b07c92
Bimsara3000 <b.m.kalupahana@gmail.com>
2025-06-20 14:41:40 +0530

    Added a comment for clarity

--- greet.py
+++ greet.py
@@ -8,7 +8,7 @@ import datetime
 #    print(greet(user)) 
 
 def greet(name):
-    now = datetime.datetime.now()
+    now = datetime.datetime.now()   #used to assign the current time to the now variable
     return f"Hello, {name}! Current time is {now:%Y-%m-%d%H:%M:%S}"
 
 if __name__ == "__main__":
